ca153faf9b4da0fa6a8dbb40c5b9093bb47c4407
JerylDEv <jeryldev@gmail.com>
2020-08-06 21:33:28 +0800

    created Nav component

--- DatingAppSPA/src/app/app.module.ts
+++ DatingAppSPA/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, ValueComponent, NavComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
