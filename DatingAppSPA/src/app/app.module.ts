75049f1178d0a163715545c87f35427780d85984
JerylDEv <jeryldev@gmail.com>
2020-08-19 17:47:49 +0800

    updated the assets folder

--- DatingAppSPA/src/app/app.module.ts
+++ DatingAppSPA/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -13,9 +14,14 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
+
+export function tokkenGetter(): string {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
@@ -25,6 +31,7 @@ import { appRoutes } from './routes';
     RegisterComponent,
     MemberListComponent,
     MemberCardComponent,
+    MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
   ],
@@ -34,6 +41,13 @@ import { appRoutes } from './routes';
     FormsModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokkenGetter,
+        allowedDomains: ['localhost:5000'],
+        disallowedRoutes: ['localhost:5000/api/auth'],
+      },
+    }),
   ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent],
--- DatingAppSPA/src/app/members/member-card/member-card.component.ts
+++ DatingAppSPA/src/app/members/member-card/member-card.component.ts
@@ -11,5 +11,5 @@ export class MemberCardComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit(): void {}
 }
